2b41df5ea514e5a60b331bdfe7fe46359e1ecf7f
younes <mraniy@gmail.com>
2023-07-15 02:32:14 +0200

    l annonce retractable dans l ecran offers by order

--- screens/stack/orders/OffersByOrder.js
+++ screens/stack/orders/OffersByOrder.js
@@ -1,34 +1,62 @@
-import {FlatList, ScrollView, StyleSheet, Text, View} from "react-native";
-import React from "react";
+import {default as Alert, Button, FlatList, ScrollView, StyleSheet, Text, View, SafeAreaView} from "react-native";
+import React, {useState} from "react";
 import UnitOfferByOrder from "./UnitOfferByOrder";
 import GlobalStyles from "../../styles/GlobalStyles";
 import HorizontalLineSeparator from "../../../components/HorizontalLineSeparator";
-import {FontAwesome, FontAwesome5, MaterialCommunityIcons} from "@expo/vector-icons";
+import {FontAwesome, FontAwesome5, MaterialCommunityIcons, MaterialIcons, Octicons} from "@expo/vector-icons";
+import {Collapse,CollapseHeader, CollapseBody, AccordionList} from 'accordion-collapse-react-native';
 
 const OffersByOrder = ({navigation}) => {
     const data = require('../../../mocks/offersByOrder');
+    const [upDownIcon, setUpDownIcon] = useState("expand-more");
+    const switchIcon = (isExpanded) => {
+        if (isExpanded === true)   setUpDownIcon ("expand-less");
+        else   setUpDownIcon ("expand-more")
+    };
     return (
         <View style={{flex: 1, flexDirection:'column', justifyContent: 'center', marginLeft:10}}>
+
             <View style={  [GlobalStyles.window]  }>
-                <Text style={  [GlobalStyles.textPlain, {fontWeight:800}]  }>{data.title}</Text>
-                <View style={{marginBottom:4}}>
-                    <HorizontalLineSeparator/>
-                </View>
 
-                <View style={ [GlobalStyles.viewRowContainer, {marginBottom:10}] }>
-                    <FontAwesome name="shopping-bag" style={GlobalStyles.iconStyle}/>
-                    <View style={ [GlobalStyles.viewColumnContainer, {marginRight:40}] }>
-                        <Text style={[GlobalStyles.textGreyed, {marginHorizontal:10, marginVertical: 4, padding:0}]}>Product price</Text>
-                        <Text style={[GlobalStyles.textBold, {marginHorizontal:10, marginVertical: 0, padding:0}]}>{data.productPrice}E</Text>
-                    </View>
+                <Collapse isExpanded={false}
+                          onToggle={switchIcon} >
 
-                    <MaterialCommunityIcons name="hand-coin"  style={GlobalStyles.iconStyle}/>
-                    <View style={ [GlobalStyles.viewColumnContainer] }>
-                        <Text style={[GlobalStyles.textGreyed, {marginHorizontal:10, marginVertical: 4, padding:0}]}>Traveller tip</Text>
-                        <Text style={[GlobalStyles.textBold, {marginHorizontal:10, marginVertical: 0, padding:0}]}>{data.fee}E</Text>
-                    </View>
-                </View>
-                <View>
+                    <CollapseHeader>
+                        <Text style={  [GlobalStyles.textPlain, {fontWeight:800}]  }>{data.title}</Text>
+                        <MaterialIcons name={upDownIcon}
+                                  style={  [GlobalStyles.iconStyle, {width:"10%", alignContent:"center", position:"absolute",
+                                      zIndex:10, right:0    }]  }  />
+                    </CollapseHeader>
+
+                    <CollapseBody>
+                        <View style={{marginBottom:4}}>
+                            <HorizontalLineSeparator/>
+                        </View>
+                        <View style={ [GlobalStyles.viewRowContainer, {marginBottom:10}] }>
+                            <FontAwesome name="shopping-bag" style={GlobalStyles.iconStyle}/>
+                            <View style={ [GlobalStyles.viewColumnContainer, {marginRight:40}] }>
+                                <Text style={[GlobalStyles.textGreyed, {marginHorizontal:10, marginVertical: 4, padding:0}]}>Product price</Text>
+                                <Text style={[GlobalStyles.textBold, {marginHorizontal:10, marginVertical: 0, padding:0}]}>{data.productPrice}E</Text>
+                            </View>
+
+                            {/*
+
+                            //DONT DELETE
+
+                            <MaterialCommunityIcons name="hand-coin"  style={GlobalStyles.iconStyle}/>
+                            <View style={ [GlobalStyles.viewColumnContainer] }>
+                                <Text style={[GlobalStyles.textGreyed, {marginHorizontal:10, marginVertical: 4, padding:0}]}>Traveller tip</Text>
+                                <Text style={[GlobalStyles.textBold, {marginHorizontal:10, marginVertical: 0, padding:0}]}>{data.fee}E</Text>
+                            </View>
+                            */}
+
+                        </View>
+
+                    </CollapseBody>
+                </Collapse>
+
+
+                <View style={{alignItems: "center"}}>
                     <View style={styles.footer}></View>
                 </View>
             </View>
@@ -43,17 +71,17 @@ const OffersByOrder = ({navigation}) => {
                                                 toCity = {item.toCity}
                               />
                           }>
-
                 </FlatList>
-            </View>
-        </View>
 
+            </View>
+
+        </View>
 
     )
 };
 const styles = StyleSheet.create({
     footer:{
-        width:"100%",
+        width:"90%",
         height:2,
         backgroundColor: "#7786C2",
     }
--- screens/stack/orders/UnitOfferByOrder.js
+++ screens/stack/orders/UnitOfferByOrder.js
@@ -1,27 +1,27 @@
-import {Image, StyleSheet, Text, View} from 'react-native';
+import {Image, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import HorizontalLineSeparator from "../../../components/HorizontalLineSeparator";
-import React from "react";
+import React, {useState} from "react";
 import GlobalStyles from "../../styles/GlobalStyles";
 import StyledButton from "../../../components/StyledButton";
 import StyledLink from "../../../components/StyledLink";
 import { Rating } from 'react-native-ratings';
-import {Entypo, FontAwesome, FontAwesome5, Fontisto, Ionicons, MaterialCommunityIcons} from "@expo/vector-icons";
+import {Entypo, FontAwesome, Fontisto, Ionicons} from "@expo/vector-icons";
+
 
 const UnitOfferByOrder = ({fee, date,traveler, fromCity, toCity}) => {
-
     return (
-        <View style={  [GlobalStyles.window]  }>
+        <View style={  [GlobalStyles.window, {borderTopLeftRadius:6, borderTopRightRadius:6, borderBottomLeftRadius:6, borderBottomRightRadius:6}, styles.box]  }>
 
             <View style={[GlobalStyles.viewRowContainer,{marginLeft:0}]}>
                 <View style={ [GlobalStyles.viewRowContainer] }>
                     <Image
-                        style={  {width:50, height:50, margin:10}  }
+                        style={  {width:50, height:50, margin:10, marginLeft:10}  }
                         source={require('../../../assets/images/Profiles/profile1.png')}
                     />
                     <View style={ [GlobalStyles.viewColumnContainer] }>
                         <Text style={[GlobalStyles.textBold,{fontSize: 15, color:"#b63ca9", marginHorizontal:0, marginTop:10}]}>{traveler.firstName}</Text>
                         <Rating
-                            showRating={false}
+                            showRoating={false}
                             imageSize={18}
                             ratingCount={5}
                             readonly={true}
@@ -31,8 +31,9 @@ const UnitOfferByOrder = ({fee, date,traveler, fromCity, toCity}) => {
 
                         />
                     </View>
+                    <Text style={[GlobalStyles.textGreyed, {marginHorizontal:0, padding:0, marginTop:40}]}> ({traveler.ratingCount})</Text>
                 </View>
-                <View style={ [GlobalStyles.viewRowContainer, {marginTop:10, marginBottom:10, marginLeft:20}]  }>
+                <View style={ [GlobalStyles.viewRowContainer, {marginTop:15, marginBottom:10, marginLeft:10}]  }>
                     <FontAwesome name="shopping-cart"  style={GlobalStyles.iconStyle}/>
                     <View style={ [GlobalStyles.viewColumnContainer] }>
                         <Text style={[GlobalStyles.textGreyed, {marginHorizontal:10, marginVertical: 4, padding:0}]}>Shops for you</Text>
@@ -42,9 +43,9 @@ const UnitOfferByOrder = ({fee, date,traveler, fromCity, toCity}) => {
                 </View>
             </View>
 
-            <View style={ [GlobalStyles.viewRowContainer, {marginTop:0, marginBottom:10, marginLeft:20}]  }>
-                <Entypo name="message"  style={[GlobalStyles.iconStyle,{marginLeft:0}]}/>
-                <StyledLink linkLabel={"Send message"} link={"Home"} smallFont={true} style={{marginLeft:5, marginTop:12}}/>
+            <View style={ [GlobalStyles.viewRowContainer, {marginTop:10, marginBottom:10, marginLeft:20}]  }>
+                <Entypo name="message"  style={[GlobalStyles.iconStyle,{marginLeft:0, paddingTop:0}]}/>
+                <StyledLink linkLabel={"Send message"} link={"Home"} smallFont={true} style={{marginLeft:5, marginTop:2}}/>
             </View>
 
             <View style={{marginTop: 5, marginBottom:10}}>
@@ -57,7 +58,7 @@ const UnitOfferByOrder = ({fee, date,traveler, fromCity, toCity}) => {
                     <Text style={[GlobalStyles.textRoute,{fontSize: 15, fontWeight:700, color:"#B43C6C", marginLeft:10, marginTop:5}]}>{fromCity}</Text>
                     <Ionicons name="airplane" style={[GlobalStyles.iconStyle, {width:"10%", color:"#0897B4", marginLeft:10, paddingTop:5}]}/>
                     <Text style={[GlobalStyles.textRoute,{fontSize: 15, fontWeight:700, color:"#B43C6C", marginHorizontal:0, marginTop:5}]}>{toCity}</Text>
-                    <View style={GlobalStyles.viewColumnContainer}>
+                    <View style={  [GlobalStyles.viewColumnContainer]  }>
                         <Text style={[GlobalStyles.textGreyed, {marginHorizontal:20, marginVertical: 0, paddingVertical:0}]}>Delivers by </Text>
                         <Text style={[GlobalStyles.textDate, {fontWeight:700, marginHorizontal:20, marginVertical: 0, paddingVertical:0}]}> >> {date} </Text>
                     </View>
@@ -68,8 +69,6 @@ const UnitOfferByOrder = ({fee, date,traveler, fromCity, toCity}) => {
                 <HorizontalLineSeparator/>
             </View>
 
-
-
             <View style={ [GlobalStyles.viewRowContainer, {marginLeft:90, marginBottom:10}]  }>
                 <StyledButton buttonLabel={"Order and pay"} buttonLink={"Home"} small={true}/>
             </View>
@@ -79,6 +78,14 @@ const UnitOfferByOrder = ({fee, date,traveler, fromCity, toCity}) => {
 };
 
 const styles = StyleSheet.create({
+    box: {
+        // ...
+        shadowColor: 'white',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.5,
+        shadowRadius: 2,
+        elevation: 2,
+    }
 });
 
 
