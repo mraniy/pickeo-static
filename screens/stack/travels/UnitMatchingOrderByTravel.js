b562fb41b8541a770b98d63c79a4865266cf9caa
younes <mraniy@gmail.com>
2023-07-24 01:10:53 +0200

    Ecran Matching orders by travel ordre des sections

--- screens/stack/travels/UnitMatchingOrderByTravel.js
+++ screens/stack/travels/UnitMatchingOrderByTravel.js
@@ -3,7 +3,6 @@ import HorizontalLineSeparator from "../../../components/HorizontalLineSeparator
 import React, {useState} from "react";
 import GlobalStyles from "../../styles/GlobalStyles";
 import {Entypo, FontAwesome, MaterialCommunityIcons} from "@expo/vector-icons";
-import StyledButton from "../../../components/StyledButton";
 import StyledLabel from "../../../components/StyledLabel";
 import NumericTextInput from "../../../components/NumercicTextInput";
 import Toggle from "react-native-toggle-element";
@@ -19,6 +18,18 @@ const UnitMatchingOrderByTravel = ({title, price, fee, shopper}) => {
     return (
         <View style={GlobalStyles.window}>
             <Text style={  [GlobalStyles.textPlain, {fontWeight:800}]  }>{title}</Text>
+            <View style={ [GlobalStyles.viewRowContainer, {marginBottom:10}] }>
+                <FontAwesome name="shopping-bag" style={GlobalStyles.iconStyle}/>
+                <View style={ [GlobalStyles.viewColumnContainer, {marginRight:40}] }>
+                    <Text style={[GlobalStyles.textGreyed, {marginHorizontal:10, marginVertical: 4, padding:0}]}>Product price</Text>
+                    <Text style={[GlobalStyles.textBold, {marginHorizontal:10, marginVertical: 0, padding:0}]}>{price}E</Text>
+                </View>
+                <MaterialCommunityIcons name="hand-coin"  style={GlobalStyles.iconStyle}/>
+                <View style={ [GlobalStyles.viewColumnContainer] }>
+                    <Text style={[GlobalStyles.textGreyed, {marginHorizontal:10, marginVertical: 4, padding:0}]}>Traveller tip</Text>
+                    <Text style={[GlobalStyles.textBold, {marginHorizontal:10, marginVertical: 0, padding:0}]}>{fee}E</Text>
+                </View>
+            </View>
             <View style={{marginBottom:4}}>
                 <HorizontalLineSeparator/>
             </View>
@@ -54,21 +65,6 @@ const UnitMatchingOrderByTravel = ({title, price, fee, shopper}) => {
                 <HorizontalLineSeparator/>
             </View>
 
-            <View style={ [GlobalStyles.viewRowContainer, {marginBottom:10}] }>
-                <FontAwesome name="shopping-bag" style={GlobalStyles.iconStyle}/>
-                <View style={ [GlobalStyles.viewColumnContainer, {marginRight:40}] }>
-                    <Text style={[GlobalStyles.textGreyed, {marginHorizontal:10, marginVertical: 4, padding:0}]}>Product price</Text>
-                    <Text style={[GlobalStyles.textBold, {marginHorizontal:10, marginVertical: 0, padding:0}]}>{price}E</Text>
-                </View>
-                <MaterialCommunityIcons name="hand-coin"  style={GlobalStyles.iconStyle}/>
-                <View style={ [GlobalStyles.viewColumnContainer] }>
-                    <Text style={[GlobalStyles.textGreyed, {marginHorizontal:10, marginVertical: 4, padding:0}]}>Traveller tip</Text>
-                    <Text style={[GlobalStyles.textBold, {marginHorizontal:10, marginVertical: 0, padding:0}]}>{fee}E</Text>
-                </View>
-            </View>
-
-            <HorizontalLineSeparator/>
-
             <View style={ [GlobalStyles.viewRowContainer, {marginLeft:30, marginTop:15, marginBottom:10}]  }>
 
                 <View style={  {marginLeft:0, marginTop:0, marginRight:30, flexDirection:"row"}  }>
@@ -86,31 +82,29 @@ const UnitMatchingOrderByTravel = ({title, price, fee, shopper}) => {
 
                 </View>
 
-                <View style={  {marginTop:5}  }>
-                <Toggle
-                    value={toggleValue}
-                    onPress={  (newState) => setToggleValue(newState)  }
-                    leftTitle = {  <Text style={GlobalStyles.toggleText}>{"No"}</Text>  }
-                    rightTitle= { <Text style={GlobalStyles.toggleText}>{"Yes"}</Text>  }
-                    trackBar={{
-                        width: 120,
-                        height: 35,
-                        radius: 25,
-                        activeBackgroundColor:"#E0A2A1",
-                        inActiveBackgroundColor:"#c24341"
+                <View style={  {marginTop:0}  }>
+                    <StyledLabel labelText={"    Bring for "+offer+"E"} style2={{marginTop:0, marginBottom:5}}></StyledLabel>
+                    <Toggle
+                        value={toggleValue}
+                        onPress={  (newState) => setToggleValue(newState)  }
+                        leftTitle = {  <Text style={GlobalStyles.toggleText}>{"No"}</Text>  }
+                        rightTitle= { <Text style={GlobalStyles.toggleText}>{"Yes"}</Text>  }
+                        trackBar={{
+                            width: 120,
+                            height: 35,
+                            radius: 25,
+                            activeBackgroundColor:"#E0A2A1",
+                            inActiveBackgroundColor:"#c24341"
+                        }}
+                        thumbButton={{
+                            width: 50,
+                            height: 35,
+                            radius: 20,
+                            activeBackgroundColor:"#3b5f73",
+                            inActiveBackgroundColor:"#3b5f73"
 
-                    }}
-
-                    thumbButton={{
-                        width: 50,
-                        height: 35,
-                        radius: 20,
-                        activeBackgroundColor:"#3b5f73",
-                        inActiveBackgroundColor:"#3b5f73"
-
-                    }}
-
-                />
+                        }}
+                    />
                 </View>
 
             </View>
